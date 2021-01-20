2cc88f58cc4150d29d6624c114c0e203dbf0ab98
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-20 17:56:38 +0530

    1 . Add a new sample .

---
+++ RadioButton_Event/RadioButton_Event/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.XForms.Buttons;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RadioButton_Event
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_StateChanged(object sender, StateChangedEventArgs e)
+        {
+            if (e.IsChecked.HasValue && e.IsChecked.Value)
+            {
+                (sender as SfRadioButton).Text = "Checked State";
+            }
+            else if (e.IsChecked.HasValue && !e.IsChecked.Value)
+            {
+                (sender as SfRadioButton).Text = "Unchecked State";
+            }
+
+        }
+
+    }
+}
